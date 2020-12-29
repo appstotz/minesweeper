dac6c0b09373dc90d7169e11b8e794b571b96fb2
appstotz <github@appstotz.rocks>
2020-12-29 15:37:34 +0100

    refactor to use shuffle instead of manuel creation of fields

--- lib/game/logic/RandomBombGenerator.dart
+++ lib/game/logic/RandomBombGenerator.dart
@@ -2,9 +2,7 @@ import 'dart:math';
 
 /// a Class to generate a random field for the bombs
 class RandomBombGenerator {
-  /// generate a new field
-  /// generate a new playing field
-
+  /// generate a new list of random bomb locations
   List<bool> _generateNewBombLocations(int width, int height, int bombCount) {
     if (width <= 0) {
       throw ArgumentError.value(width);
@@ -21,25 +19,13 @@ class RandomBombGenerator {
     final random = Random();
     //  reduce 1 because causing the player to tap on a bomb at the beginning is no fun
     final neededFields = (width * height) - 1;
-    List<int> bombLocations = List.empty(growable: true);
-    for (var i = 0; i < bombCount; i++) {
-      int newRandom = random.nextInt(neededFields);
-      while (true) {
-        if (!bombLocations.contains(newRandom)) {
-          break;
-        }
-        newRandom++;
-        if (newRandom >= neededFields) {
-          newRandom = 0;
-        }
-      }
-      bombLocations.add(newRandom);
-    }
     final List<bool> list =
-        List.generate(neededFields, (index) => bombLocations.contains(index));
+        List.generate(neededFields, (index) => index < bombCount);
+    list.shuffle(random);
     return list;
   }
 
+  /// generate a new playing field
   List<List<bool>> generateNewField(
     int width,
     int height,
