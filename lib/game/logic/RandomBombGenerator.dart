10f10a99795d97b3e52dfb533c85f3c8f58ce050
appstotz <github@appstotz.rocks>
2021-01-03 16:50:48 +0100

    extend bomb free zone to all surrounding fields of the starting field

--- lib/game/logic/RandomBombGenerator.dart
+++ lib/game/logic/RandomBombGenerator.dart
@@ -3,7 +3,13 @@ import 'dart:math';
 /// a Class to generate a random field for the bombs
 class RandomBombGenerator {
   /// generate a new list of random bomb locations
-  List<bool> _generateNewBombLocations(int width, int height, int bombCount) {
+  List<bool> _generateNewBombLocations(
+    int width,
+    int height,
+    int bombCount,
+    int firstClickX,
+    int firstClickY,
+  ) {
     if (width <= 0) {
       throw ArgumentError.value(width);
     }
@@ -16,9 +22,19 @@ class RandomBombGenerator {
     if ((width * height) <= bombCount) {
       throw ArgumentError("bomb count to high");
     }
+    var reduceNeededFields = 9;
+    if (firstClickX == 0 || firstClickX == width - 1) {
+      if (firstClickY == 0 || firstClickY == height - 1) {
+        reduceNeededFields = 4;
+      } else {
+        reduceNeededFields = 6;
+      }
+    } else if (firstClickY == 0 || firstClickY == height - 1) {
+      reduceNeededFields = 6;
+    }
     final random = Random();
     //  reduce 1 because causing the player to tap on a bomb at the beginning is no fun
-    final neededFields = (width * height) - 1;
+    final neededFields = (width * height) - reduceNeededFields;
     final List<bool> list =
         List.generate(neededFields, (index) => index < bombCount);
     list.shuffle(random);
@@ -39,9 +55,13 @@ class RandomBombGenerator {
     if (firstClickX < 0 || firstClickX >= width) {
       throw ArgumentError.value(firstClickX);
     }
-    final bombLocations = _generateNewBombLocations(width, height, bombCount);
-    final addPosition = (firstClickX * height) + firstClickY;
-    bombLocations.insert(addPosition, false);
+    final bombLocations = _generateNewBombLocations(
+        width, height, bombCount, firstClickX, firstClickY);
+
+    //  add the missing positions
+    _addPositions(firstClickX, height, firstClickY, width, bombLocations);
+
+    // generate  the list
     final generatedList = List.generate(
       width,
       (x) => List.generate(
@@ -50,5 +70,43 @@ class RandomBombGenerator {
       ),
     );
     return generatedList;
+  }
+
+  /// add the missing positions
+  void _addPositions(int firstClickX, int height, int firstClickY, int width, List<bool> bombLocations) {
+    var positions =
+        List.of([(firstClickX * height) + firstClickY], growable: true);
+    var top = firstClickY == 0;
+    var start = firstClickX == 0;
+    var bottom = firstClickY == height - 1;
+    var end = firstClickX == width - 1;
+    if (!top) {
+      positions.add((firstClickX * height) + (firstClickY - 1));
+      if (!start) {
+        positions.add(((firstClickX - 1) * height) + (firstClickY - 1));
+      }
+      if (!end) {
+        positions.add(((firstClickX + 1) * height) + (firstClickY - 1));
+      }
+    }
+    if (!bottom) {
+      positions.add((firstClickX * height) + (firstClickY + 1));
+      if (!start) {
+        positions.add(((firstClickX - 1) * height) + (firstClickY + 1));
+      }
+      if (!end) {
+        positions.add(((firstClickX + 1) * height) + (firstClickY + 1));
+      }
+    }
+    if (!start) {
+      positions.add(((firstClickX - 1) * height) + firstClickY);
+    }
+    if (!end) {
+      positions.add(((firstClickX + 1) * height) + firstClickY);
+    }
+    positions.sort((a, b) => a - b);
+    positions.forEach((element) {
+      bombLocations.insert(element, false);
+    });
   }
 }
