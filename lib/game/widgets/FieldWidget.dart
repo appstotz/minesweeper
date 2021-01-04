1a08a7b61e38aad5143ff3984ad2615f13082ddc
appstotz <github@appstotz.rocks>
2021-01-04 20:26:15 +0100

    implement scaling of the Field Widgets to better support different device sizes

--- lib/game/widgets/FieldWidget.dart
+++ lib/game/widgets/FieldWidget.dart
@@ -9,10 +9,8 @@ class FieldWidget extends StatelessWidget {
   /// the constructor
   const FieldWidget(this._field);
 
-  /// size of the field (TODO: move to another place?)
-  final double _size = 10;
-
-  final _borderColor = Colors.black;
+  /// the color of the boarder
+  static const _borderColor = Colors.black;
 
   @override
   Widget build(BuildContext context) {
@@ -24,64 +22,64 @@ class FieldWidget extends StatelessWidget {
       if (_field.totalBombs == 0) {
         content = null;
       } else if (_field.bomb) {
-        content = _bombWidget();
+        content = _bombWidget;
       } else {
         content = _bombCountWidget(_field.totalBombs);
       }
       final Decoration decoration =
           BoxDecoration(color: Colors.white, border: border);
       return Container(
-        width: _size,
-        height: _size,
         decoration: decoration,
-        child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
           child: content,
         ),
       );
     } else {
       if (_field.flagged) {
-        content = _flaggedField();
+        content = _flaggedField;
       } else {
         content = null;
       }
       return Material(
         elevation: 5,
-        child: Container(
-          color: Colors.grey,
-          child: Center(
-            child: content,
-          ),
+        color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: content,
         ),
       );
     }
   }
 
   /// the widget for the bomb
-  Widget _bombWidget() {
-    return Icon(
+  static const Widget _bombWidget = FittedBox(
+    fit: BoxFit.fill,
+    child: Icon(
       Icons.whatshot_outlined,
-      size: 35,
       color: Colors.red,
-    );
-  }
+    ),
+  );
 
   /// the widget for the number of bombs
   Widget _bombCountWidget(int count) {
-    return Text(
-      count.toString(),
-      style: TextStyle(
-        fontSize: 35,
-        color: Colors.amber,
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: Text(
+        count.toString(),
+        style: TextStyle(
+          color: Colors.amber,
+        ),
       ),
     );
   }
 
   /// the widget for flagged field
-  Widget _flaggedField() {
-    return Icon(
+  static const Widget _flaggedField = FittedBox(
+    fit: BoxFit.fill,
+    child: Icon(
       Icons.flag,
-      size: 35,
       color: Colors.red,
-    );
-  }
+    ),
+  );
 }
